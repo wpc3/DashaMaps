5a9669c60baa791a8edc245b27d6e379bacdbd5f
Will <will@Zipcoders-MacBook-Pro-9.local>
2024-07-26 08:00:30 -0400

    made changes

---
+++ src/main/java/com/github/zipcodewilmington/Node.java
@@ -0,0 +1,14 @@
+package com.github.zipcodewilmington;
+
+public class Node {
+    String k;
+    Integer v;
+    Node next;
+
+    Node(String k, Integer v){
+        this.k = k;
+        this.v = v;
+        this.next = null;
+    }
+
+}
